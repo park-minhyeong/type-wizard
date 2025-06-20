114502979d732f2d2cde079bb09177fc4a43e04d
minhyeong <minhyeong.dev@gmail.com>
2025-06-20 13:48:27 +0900

    fix: no null

--- src/types/TypeGuard.ts
+++ src/types/TypeGuard.ts
@@ -1,15 +1,42 @@
-export interface Schema { [key: string]: TypeDescriptor }
+export interface Schema {
+  [key: string]: TypeDescriptor;
+}
 
 export type TypeDescriptor =
-	| { type: 'string'; optional?: boolean; nullable?: boolean; enum?: readonly unknown[] }
-	| { type: 'number'; optional?: boolean; nullable?: boolean; enum?: readonly unknown[] }
-	| { type: 'boolean'; optional?: boolean; nullable?: boolean; enum?: readonly unknown[] }
-	| { type: 'date'; optional?: boolean; nullable?: boolean }
-	| { type: 'object'; optional?: boolean; nullable?: boolean; of: Schema | ((v: unknown) => boolean) }
-	| { type: 'array'; optional?: boolean; nullable?: boolean; of: TypeDescriptor | ((v: unknown) => boolean) };
+  | {
+      type: "string";
+      optional?: boolean;
+      nullable?: boolean;
+      enum?: readonly unknown[];
+    }
+  | {
+      type: "number";
+      optional?: boolean;
+      nullable?: boolean;
+      enum?: readonly unknown[];
+    }
+  | {
+      type: "boolean";
+      optional?: boolean;
+      nullable?: boolean;
+      enum?: readonly unknown[];
+    }
+  | { type: "date"; optional?: boolean; nullable?: boolean }
+  | {
+      type: "object";
+      optional?: boolean;
+      nullable?: boolean;
+      of: Schema | ((v: unknown) => boolean);
+    }
+  | {
+      type: "array";
+      optional?: boolean;
+      nullable?: boolean;
+      of: TypeDescriptor | ((v: unknown) => boolean);
+    };
 
 export interface TypeGuard<T> {
-	(value: unknown): value is T;
-	optional(): TypeGuard<T | null>;
-	message(val: unknown): string | null;
+  (value: unknown): value is T;
+  optional(): TypeGuard<T>;
+  message(val: unknown): string | null;
 }
